5ed0507a1fa47e0cbdf2337aef679f0807ead522
AntCannon <anthonycannonier@gmail.com>
2024-06-23 09:30:38 -0400

    Add form inputs.
    1. Name, date, duration, calories burned.
    2. Style to middle of page.

--- src/components/createWorkout/CreateWorkout.js
+++ src/components/createWorkout/CreateWorkout.js
@@ -1,17 +1,38 @@
+import './CreateWorkout.css'
 import React from "react";
+import { useState } from 'react'
 
 const CreateWorkout = ({ addWorkout }) => {
+  const [ workoutForm, setWorkoutform ] = useState({
+    name: "",
+    date: "",
+    duration: "",
+    caloriesBurned: 0
+  })
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div>
+    <section className="create-workout-form">
       <h2>Create Workout</h2>
       <form onSubmit={handleSubmit}>
+        <label>
+          Name: <input type="text" placeholder="Workout name" />
+        </label>
+        <label>
+          Date: <input type="date" />
+        </label>
+        <label>
+          Duration (mins): <input type="number" />
+        </label>
+        <label>
+          Calories Burned: <input type="number" />
+        </label>
         <button type="submit">Create</button>
       </form>
-    </div>
+    </section>
   );
 };
 
